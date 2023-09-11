fd20409dbf59e0fe9802e36bcf601640fe6bc338
LeonieNollevalle <leonie.nollevalle@gmail.com>
2023-09-11 15:19:46 +0200

    suite

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
 import { useState,useEffect } from 'react';
 import './App.css';
 import { db } from "./firebase-config";
-import { uid } from "uid";
+// import { uid } from "uid";
 import { set, ref, onValue} from "@firebase/database";
+import Letter from './Letter';
 
 
 function App() {
   // const [form, setForm] = useState (false)
   const [ideas, setIdeas] = useState ([])
-  const [adress, setAdress] = useState('')
-  const [code, setCode] = useState('')
-  const [categorie, setCategorie] = useState('')
-  const [title, setTitle] = useState('')
-  const [exple, setExple] = useState('')
+  // const [adress, setAdress] = useState('')
+  // const [code, setCode] = useState('')
+  // const [categorie, setCategorie] = useState('')
+  // const [title, setTitle] = useState('')
+  // const [exple, setExple] = useState('')
+  const [ projet, setProjet ] = useState (0)
 
 
-
-console.log(ideas)
+  
 
 // Read
-  useEffect (()=> {
+  useEffect (() => {
     onValue(ref(db), snapshot => {
       const data = snapshot.val();
-      
-      if (data !== null){
-        Object.values(data).map(name => (
+      if (data != null){
+        Object.values(data).map((name) => (
           setIdeas(oldArray => [...oldArray, name])
     ))
       }
     })
-  }, []);
+  },[]);
+ 
 
+  console.log(ideas)
 // write
 // const writeToDatabase = () => {
 //   const uuid = uid();
@@ -50,10 +52,12 @@ console.log(ideas)
 //   setForm(false)
 // };
 const font = ['','serif', 'sans-serif'];
-const color = ['','red','blue'];
+const color = ['','red','red'];
+
 const chooseColor = () => {
  return color[Math.floor(Math.random() * (2 - 1 + 1)) + 1]
 }
+
   return (
     <div className="w-screen h-screen flex" style={{ fontFamily:`${font[Math.floor(Math.random() * (2 - 1 + 1)) + 1]}`,color:`${chooseColor()}`}}>
       <div className="w-1/2 p-3 flex gap-5">
@@ -63,13 +67,15 @@ const chooseColor = () => {
             <p className="text-sm pt-5 flex flex-col"> Explication du projet A lighthearted mixture of generalities, carefully distilled. A delightful collection of the slightly scattered and mostly sweet.  Why bury? How are myself?  To catch a glimpse of intimacy, to stay, just a minute longer. There are no simple answers, or? <p className="text-lg font-bold text-end ">↓</p></p>
           </div>
          
-           <div className="w-full flex  flex-col items-end justify-between ">
+           <div className="w-full flex  flex-col items-end justify-between">
            <ul className="w-1/2  pt-10">
              <li className="pb-3 underline">Omnicity</li>
-             {}
-             <ul>
-             <li className="pl-3 flex gap-3 items-center">Logo aléatoire 1.0 <p className="text-lg font-bold ">→</p></li>
-             </ul>
+             {ideas.map((idea)=> (
+               <ul>
+                <li className="pl-3 flex gap-3 items-center cursor-pointer" onClick={() => setProjet(idea.id)}>{idea.title ? idea.title : "coucou"}<p className="text-lg font-bold "> {idea.id === projet ? '→' : ''}</p></li>
+               </ul>
+             ))}
+             
            </ul>
            {/* <ul className=" pt-10">
              <li className="pb-3 underline">GJ</li>
@@ -84,22 +90,22 @@ const chooseColor = () => {
          </div>
          <div className="w-1/2">
            <h2 className="italic">Omnicity</h2>
-            <p className='text-sm pt-5'>Explication de ce qu'est ominicity et de pourquoi il a été choisie</p>
-            <h3 className="pt-5 flex gap-3 items-center">Logo Aléatoire 1.0 <p className="text-lg font-bold ">→</p></h3>
-            <p className="text-sm pt-2">Explication hyper intéressante sur ce qui a été mener comme recherche. Ça viens de tel référence artistique nanani nanana</p>
+            <p className='text-sm pt-5'>Omnicity est une entreprise d’architecture qui propose des aménagements d’intérieure virtuelle destiné aux agences immobilières dans le but de les aidés à vendre leur bien. Elle est ici utilisée comme support d’expérimentation d’une identité visuelle généré à partir de programme informatique. Afin de mener à bien cette expérimentation, un jeu de caractère modulaire a été créé au préalable afin de composer le nom de l’entreprise. <br/> Ces lettres prennent l’apparence de plan d’architecture en référence à l’activité principale de l’entreprise qui consiste à réaliser des plans d’aménagement, des lettres qui ne permettent pas dans un premier de distinguer le nom de l’entreprise surtout si elles sont disposées de manière à brouiller la lecture de ces dernières. De plus, chacune des lettres est pensée à partir d’une grille commune à toute, tout comme en architecture des modules sont utilisé pour composer l’espace et le positionnement des lettres. </p>
+            <Letter/>
+            <h3 className="pt-5 flex gap-3 items-center">{ideas[projet] ? ideas[projet].title : "coucou"}<p className="text-lg font-bold ">→</p></h3>
+            <p className="text-sm pt-2">{ideas[projet] ? ideas[projet].exple :''}</p>
          </div>
-         
       </div>
       
       <div className="w-1/2 h-full p-3 ">
-        <h2 className="italic flex gap-3">Logo Aléatoire 1.0 <p className="text-lg font-bold not-italic ">↓</p></h2>
-        <div className="w-full h-1/2 flex justify-center items-center" style={{ border:`1px solid ${chooseColor()}`}}>
+        <h2 className="italic flex gap-3">{ideas[projet] ? ideas[projet].title : "coucou"} <p className="text-lg font-bold not-italic ">↓</p></h2>
+        <div className="w-full h-1/2 flex justify-center items-center mt-3" style={{ border:`1px solid ${chooseColor()}`}}>
           <iframe
             id="inlineFrameExample"
             title="Inline Frame Example"
             width="100%"
             height="100%"
-            src={ideas[0] ? ideas[0].adress : "https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik"}
+            src={ideas[projet] ? ideas[projet].adress : "https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik"}
           >
           </iframe>   
         </div>
---
+++ src/Letter.jsx
@@ -0,0 +1,112 @@
+import React from 'react'
+
+const Letter = () => {
+  return (
+    <div className="w-full flex gap-2 pt-5">
+      <svg width="50" height="50" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_102_6)">
+          <path d="M73 1H1V73H73V1Z" stroke="red" stroke-width="2" stroke-miterlimit="10"/>
+          <path d="M37 19V55" stroke="red" stroke-width="2" stroke-miterlimit="10"/>
+          </g>
+          <defs>
+          <clipPath id="clip0_102_6">
+          <rect width="74" height="74" fill="white"/>
+          </clipPath>
+        </defs>
+      </svg>
+
+      <svg width="50" height="50" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_102_9)">
+        <path d="M1 1H19H73V73H1V1Z" stroke="red" stroke-width="2" stroke-miterlimit="10"/>
+        <path d="M28 73V37" stroke="red" stroke-width="2" stroke-miterlimit="10"/>
+        <path d="M37 37V1" stroke="red" stroke-width="2" stroke-miterlimit="10"/>
+        <path d="M46 73V37" stroke="red" stroke-width="2" stroke-miterlimit="10"/>
+        </g>
+        <defs>
+        <clipPath id="clip0_102_9">
+        <rect width="74" height="74" fill="white"/>
+        </clipPath>
+        </defs>
+      </svg>
+      <h3 className="pt-3 text-xl">→</h3>
+      <svg width="50" height="50" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_103_14)">
+        <path d="M28 73V37" stroke="red" stroke-width="2" stroke-miterlimit="10"/>
+        <path d="M46 37V1" stroke="red" stroke-width="2" stroke-miterlimit="10"/>
+        <path d="M73 1H1V73H73V1Z" stroke="red" stroke-width="2" stroke-miterlimit="10"/>
+        </g>
+        <defs>
+        <clipPath id="clip0_103_14">
+        <rect width="74" height="74" fill="white"/>
+        </clipPath>
+        </defs>
+      </svg>
+
+      <svg width="50" height="50" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_103_18)">
+        <path d="M1 1H73V28V73H1V28V1Z" stroke="red" stroke-width="2" stroke-miterlimit="10"/>
+        <path d="M73 28H1" stroke="red" stroke-width="2" stroke-miterlimit="10"/>
+        </g>
+        <defs>
+        <clipPath id="clip0_103_18">
+        <rect width="74" height="74" fill="white"/>
+        </clipPath>
+        </defs>
+      </svg>
+
+      <svg width="50" height="50" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_103_21)">
+        <path d="M1 1H73V37V73H1V1Z" stroke="red" stroke-width="2" stroke-miterlimit="10"/>
+        <path d="M37 37H73" stroke="red" stroke-width="2" stroke-miterlimit="10"/>
+        </g>
+        <defs>
+        <clipPath id="clip0_103_21">
+        <rect width="74" height="74" fill="white"/>
+        </clipPath>
+        </defs>
+      </svg>
+      <h3 className="pt-3 text-xl">↓</h3>
+      <svg width="50" height="50" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_103_24)">
+        <path d="M1 1H73V28V73H1V28V1Z" stroke="red" stroke-width="2" stroke-miterlimit="10"/>
+        <path d="M73 28H1" stroke="red" stroke-width="2" stroke-miterlimit="10"/>
+        </g>
+        <defs>
+        <clipPath id="clip0_103_24">
+        <rect width="74" height="74" fill="white"/>
+        </clipPath>
+        </defs>
+      </svg>
+
+      <svg width="50" height="50" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_103_27)">
+        <path d="M1 1H73V28V73H1V28V1Z" stroke="red" stroke-width="2" stroke-miterlimit="10"/>
+        <path d="M1 46H19V73" stroke="red" stroke-width="2" stroke-miterlimit="10"/>
+        <path d="M73 46H55V73" stroke="red" stroke-width="2" stroke-miterlimit="10"/>
+        </g>
+        <defs>
+        <clipPath id="clip0_103_27">
+        <rect width="74" height="74" fill="white"/>
+        </clipPath>
+        </defs>
+      </svg>
+
+      <svg width="50" height="50" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_103_31)">
+        <path d="M1 46H19V73" stroke="red" stroke-width="2" stroke-miterlimit="10"/>
+        <path d="M73 46H55V73" stroke="red" stroke-width="2" stroke-miterlimit="10"/>
+        <path d="M1 1H73V28V73H1V28V1Z" stroke="red" stroke-width="2" stroke-miterlimit="10"/>
+        <path d="M37 1V37" stroke="red" stroke-width="2" stroke-miterlimit="10"/>
+        </g>
+        <defs>
+        <clipPath id="clip0_103_31">
+        <rect width="74" height="74" fill="white"/>
+        </clipPath>
+        </defs>
+      </svg>
+
+    </div>
+  )
+}
+
+export default Letter
