04a3309d7e8f9d75d1727152c8e1d14effe35e04
LeonieNollevalle <leonie.nollevalle@gmail.com>
2023-09-18 17:00:05 +0200

    biggest plan_2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./styles.css";
 export default function App() {
 
 let words = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18"]; 
-let tableLength = words.length;
 
   let tableau = []; 
   for (let i = 0 ; i < 9 ; i++) {
