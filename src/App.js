c35d6ae90e8589797ad18d94717b21f9359ee919
LeonieNollevalle <leonie.nollevalle@gmail.com>
2023-09-18 16:51:37 +0200

    biggest plan

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./styles.css";
 export default function App() {
 
-let words = ["1","2","3","4","5","6","7","8","9"]; 
+let words = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18"]; 
 let tableLength = words.length;
 
   let tableau = []; 
-  for (let i = 0 ; i < tableLength ; i++) {
+  for (let i = 0 ; i < 9 ; i++) {
   let max = words.length - 1;
   let min = 0;
   
@@ -24,7 +24,7 @@ console.log(tableau)
         <g clip-path="url(#clip0_102_6)">
         <path d="M73 1H1V73H73V1Z" stroke="re" stroke-width="1" stroke-miterlimit="10"/>
         <path d="M37 19V55" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
-        <path d="M73 1H1V73H73V1Z" stroke="red" stroke-width="0" stroke-miterlimit="10"/>
+        <path d="M73 1H1V73H73V1Z" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
 
         </g>
         <defs>
@@ -35,7 +35,7 @@ console.log(tableau)
       </svg>
       <svg className={`num-${tableau[1]}`} width="74" height="74" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g clip-path="url(#clip0_102_9)">
-        <path d="M1 1H19H73V73H1V1Z" stroke="red" stroke-width="0" stroke-miterlimit="10"/>
+        <path d="M1 1H19H73V73H1V1Z" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
         <path d="M28 73V37" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
         <path d="M37 37V1" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
         <path d="M46 73V37" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
@@ -51,7 +51,7 @@ console.log(tableau)
         <g clip-path="url(#clip0_103_14)">
         <path d="M28 73V37" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
         <path d="M46 37V1" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
-        <path d="M73 1H1V73H73V1Z" stroke="red" stroke-width="0" stroke-miterlimit="10"/>
+        <path d="M73 1H1V73H73V1Z" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
         </g>
         <defs>
         <clipPath id="clip0_103_14">
@@ -62,7 +62,7 @@ console.log(tableau)
 
       <svg className={`num-${tableau[3]}`} width="74" height="74" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g clip-path="url(#clip0_103_18)">
-        <path d="M1 1H73V28V73H1V28V1Z" stroke="red" stroke-width="0" stroke-miterlimit="10"/>
+        <path d="M1 1H73V28V73H1V28V1Z" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
         <path d="M73 28H1" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
         </g>
         <defs>
@@ -73,7 +73,7 @@ console.log(tableau)
       </svg>
       <svg className={`num-${tableau[4]}`} width="74" height="74" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g clip-path="url(#clip0_103_21)">
-        <path d="M1 1H73V37V73H1V1Z" stroke="red" stroke-width="0" stroke-miterlimit="10"/>
+        <path d="M1 1H73V37V73H1V1Z" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
         <path d="M37 37H73" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
         </g>
         <defs>
@@ -85,7 +85,7 @@ console.log(tableau)
 
       <svg className={`num-${tableau[5]}`} width="74" height="74" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g clip-path="url(#clip0_103_24)">
-        <path d="M1 1H73V28V73H1V28V1Z" stroke="red" stroke-width="0" stroke-miterlimit="10"/>
+        <path d="M1 1H73V28V73H1V28V1Z" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
         <path d="M73 28H1" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
         </g>
         <defs>
@@ -96,7 +96,7 @@ console.log(tableau)
       </svg>
       <svg className={`num-${tableau[6]}`} width="74" height="74" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g clip-path="url(#clip0_103_27)">
-        <path d="M1 1H73V28V73H1V28V1Z" stroke="red" stroke-width="0" stroke-miterlimit="10"/>
+        <path d="M1 1H73V28V73H1V28V1Z" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
         <path d="M1 46H19V73" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
         <path d="M73 46H55V73" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
         </g>
@@ -111,7 +111,7 @@ console.log(tableau)
         <g clip-path="url(#clip0_103_31)">
         <path d="M1 46H19V73" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
         <path d="M73 46H55V73" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
-        <path d="M1 1H73V28V73H1V28V1Z" stroke="red" stroke-width="0" stroke-miterlimit="10"/>
+        <path d="M1 1H73V28V73H1V28V1Z" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
         <path d="M37 1V37" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
         </g>
         <defs>
