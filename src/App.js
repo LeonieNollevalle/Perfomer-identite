a49e7dea933f61cd3cb963be2347a2306bc3c2ee
LeonieNollevalle <leonie.nollevalle@gmail.com>
2023-09-19 18:21:03 +0200

    new one

--- src/App.js
+++ src/App.js
@@ -23,8 +23,8 @@ console.log(tableau)
       <svg className={`num-${tableau[0]}`} width="74" height="74" stroke-width="1" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g clip-path="url(#clip0_102_6)">
         <path d="M73 1H1V73H73V1Z" stroke="re" stroke-width="1" stroke-miterlimit="10"/>
-        <path d="M37 19V55" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
-        <path d="M73 1H1V73H73V1Z" stroke="red" stroke-width="0" stroke-miterlimit="10"/>
+        <path d="M37 19V55" stroke="white" stroke-width="1" stroke-miterlimit="10"/>
+        <path d="M73 1H1V73H73V1Z" stroke="white" stroke-width="0" stroke-miterlimit="10"/>
 
         </g>
         <defs>
@@ -35,10 +35,10 @@ console.log(tableau)
       </svg>
       <svg className={`num-${tableau[1]}`} width="74" height="74" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g clip-path="url(#clip0_102_9)">
-        <path d="M1 1H19H73V73H1V1Z" stroke="red" stroke-width="0" stroke-miterlimit="10"/>
-        <path d="M28 73V37" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
-        <path d="M37 37V1" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
-        <path d="M46 73V37" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
+        <path d="M1 1H19H73V73H1V1Z" stroke="white" stroke-width="0" stroke-miterlimit="10"/>
+        <path d="M28 73V37" stroke="white" stroke-width="1" stroke-miterlimit="10"/>
+        <path d="M37 37V1" stroke="white" stroke-width="1" stroke-miterlimit="10"/>
+        <path d="M46 73V37" stroke="white" stroke-width="1" stroke-miterlimit="10"/>
         </g>
         <defs>
         <clipPath id="clip0_102_9">
@@ -49,9 +49,9 @@ console.log(tableau)
 
       <svg className={`num-${tableau[2]}`} width="74" height="74" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g clip-path="url(#clip0_103_14)">
-        <path d="M28 73V37" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
-        <path d="M46 37V1" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
-        <path d="M73 1H1V73H73V1Z" stroke="red" stroke-width="0" stroke-miterlimit="10"/>
+        <path d="M28 73V37" stroke="white" stroke-width="1" stroke-miterlimit="10"/>
+        <path d="M46 37V1" stroke="white" stroke-width="1" stroke-miterlimit="10"/>
+        <path d="M73 1H1V73H73V1Z" stroke="white" stroke-width="0" stroke-miterlimit="10"/>
         </g>
         <defs>
         <clipPath id="clip0_103_14">
@@ -62,8 +62,8 @@ console.log(tableau)
 
       <svg className={`num-${tableau[3]}`} width="74" height="74" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g clip-path="url(#clip0_103_18)">
-        <path d="M1 1H73V28V73H1V28V1Z" stroke="red" stroke-width="0" stroke-miterlimit="10"/>
-        <path d="M73 28H1" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
+        <path d="M1 1H73V28V73H1V28V1Z" stroke="white" stroke-width="0" stroke-miterlimit="10"/>
+        <path d="M73 28H1" stroke="white" stroke-width="1" stroke-miterlimit="10"/>
         </g>
         <defs>
         <clipPath id="clip0_103_18">
@@ -73,8 +73,8 @@ console.log(tableau)
       </svg>
       <svg className={`num-${tableau[4]}`} width="74" height="74" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g clip-path="url(#clip0_103_21)">
-        <path d="M1 1H73V37V73H1V1Z" stroke="red" stroke-width="0" stroke-miterlimit="10"/>
-        <path d="M37 37H73" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
+        <path d="M1 1H73V37V73H1V1Z" stroke="white" stroke-width="0" stroke-miterlimit="10"/>
+        <path d="M37 37H73" stroke="white" stroke-width="1" stroke-miterlimit="10"/>
         </g>
         <defs>
         <clipPath id="clip0_103_21">
@@ -85,8 +85,8 @@ console.log(tableau)
 
       <svg className={`num-${tableau[5]}`} width="74" height="74" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g clip-path="url(#clip0_103_24)">
-        <path d="M1 1H73V28V73H1V28V1Z" stroke="red" stroke-width="0" stroke-miterlimit="10"/>
-        <path d="M73 28H1" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
+        <path d="M1 1H73V28V73H1V28V1Z" stroke="white" stroke-width="0" stroke-miterlimit="10"/>
+        <path d="M73 28H1" stroke="white" stroke-width="1" stroke-miterlimit="10"/>
         </g>
         <defs>
         <clipPath id="clip0_103_24">
@@ -96,9 +96,9 @@ console.log(tableau)
       </svg>
       <svg className={`num-${tableau[6]}`} width="74" height="74" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g clip-path="url(#clip0_103_27)">
-        <path d="M1 1H73V28V73H1V28V1Z" stroke="red" stroke-width="0" stroke-miterlimit="10"/>
-        <path d="M1 46H19V73" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
-        <path d="M73 46H55V73" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
+        <path d="M1 1H73V28V73H1V28V1Z" stroke="white" stroke-width="0" stroke-miterlimit="10"/>
+        <path d="M1 46H19V73" stroke="white" stroke-width="1" stroke-miterlimit="10"/>
+        <path d="M73 46H55V73" stroke="white" stroke-width="1" stroke-miterlimit="10"/>
         </g>
         <defs>
         <clipPath id="clip0_103_27">
@@ -109,10 +109,10 @@ console.log(tableau)
 
       <svg className={`num-${tableau[7]}`} width="74" height="74" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g clip-path="url(#clip0_103_31)">
-        <path d="M1 46H19V73" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
-        <path d="M73 46H55V73" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
-        <path d="M1 1H73V28V73H1V28V1Z" stroke="red" stroke-width="0" stroke-miterlimit="10"/>
-        <path d="M37 1V37" stroke="red" stroke-width="1" stroke-miterlimit="10"/>
+        <path d="M1 46H19V73" stroke="white" stroke-width="1" stroke-miterlimit="10"/>
+        <path d="M73 46H55V73" stroke="white" stroke-width="1" stroke-miterlimit="10"/>
+        <path d="M1 1H73V28V73H1V28V1Z" stroke="white" stroke-width="0" stroke-miterlimit="10"/>
+        <path d="M37 1V37" stroke="white" stroke-width="1" stroke-miterlimit="10"/>
         </g>
         <defs>
         <clipPath id="clip0_103_31">
