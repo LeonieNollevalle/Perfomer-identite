c1ca70de5dfd5f34944e63e6f3aef0ec2a8a0102
LeonieNollevalle <leonie.nollevalle@gmail.com>
2023-10-25 18:14:44 +0200

    fixed link

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ export default function App() {
 
   return (
     <div className="global-container">
-      <iframe className="model" src='https://my.spline.design/untitled-62cbd1b47948680a5ad5e717d850ec76/' title='lala' frameborder='0' width='100%' height='90%'></iframe>
-      <iframe  title="lala" src="https://indd.adobe.com/embed/352667b4-5014-4007-86c3-dad4e5a89f62?startpage=1&allowFullscreen=false" width="101%" height="101%" frameborder="0" allowfullscreen=""></iframe></div>
+      <iframe className="model" src='https://my.spline.design/untitled-62cbd1b47948680a5ad5e717d850ec76/' title='lala' frameborder='0' width='100%' height='80%'></iframe>
+      <iframe  title="lala" src="https://indd.adobe.com/embed/fa17ce98-953b-489a-8025-7f90db00b987?startpage=1&allowFullscreen=false"  width="101%" height="101%" frameborder="0" allowfullscreen=""></iframe></div>
   );
 }
